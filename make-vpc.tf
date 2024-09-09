25eb3e88a9e32b21642791af62ef7dd0e1ad14b6
boiledsteak <timothymxa@gmail.com>
2024-09-09 17:08:09 +0800

    add route table

--- make-vpc.tf
+++ make-vpc.tf
@@ -5,11 +5,47 @@ resource "aws_vpc" "vpc1" {
   }
 }
 
+# Define the Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc1.id
+  tags = {
+    Name = "my-internet-gateway"
+  }
+}
+
+# Define the Route Table for Public Subnets
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+# Associate the Route Table with Public Subnets
+resource "aws_route_table_association" "public-1a" {
+  subnet_id      = aws_subnet.public-1a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public-1b" {
+  subnet_id      = aws_subnet.public-1b.id
+  route_table_id = aws_route_table.public.id
+}
+
+
+
 resource "aws_subnet" "public-1a" {
   vpc_id            = aws_vpc.vpc1.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-southeast-1a"
   map_public_ip_on_launch = true
+  
   tags = {
     Name = "public-1a"
   }
