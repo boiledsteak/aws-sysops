c6f546abf540436e9627bd1150b7a657085b57db
boiledsteak <timothymxa@gmail.com>
2024-09-09 16:55:33 +0800

    Update make-vpc.tf

--- make-vpc.tf
+++ make-vpc.tf
@@ -9,6 +9,7 @@ resource "aws_subnet" "public-1a" {
   vpc_id            = aws_vpc.vpc1.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-southeast-1a"
+  map_public_ip_on_launch = true
   tags = {
     Name = "public-1a"
   }
@@ -27,6 +28,7 @@ resource "aws_subnet" "public-1b" {
   vpc_id            = aws_vpc.vpc1.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "ap-southeast-1b"
+  map_public_ip_on_launch = true
   tags = {
     Name = "public-1b"
   }
