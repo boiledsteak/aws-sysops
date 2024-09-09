d95184bfd36af12cfd936d0ae91d3953e0299268
boiledsteak <timothymxa@gmail.com>
2024-09-09 16:27:39 +0800

    added tf

---
+++ make-vpc.tf
@@ -0,0 +1,42 @@
+resource "aws_vpc" "vpc1" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "vpc1"
+  }
+}
+
+resource "aws_subnet" "public-1a" {
+  vpc_id            = aws_vpc.vpc1.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-southeast-1a"
+  tags = {
+    Name = "public-1a"
+  }
+}
+
+resource "aws_subnet" "private-1a" {
+  vpc_id            = aws_vpc.vpc1.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "ap-southeast-1a"
+  tags = {
+    Name = "private-1a"
+  }
+}
+
+resource "aws_subnet" "public-1b" {
+  vpc_id            = aws_vpc.vpc1.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-southeast-1b"
+  tags = {
+    Name = "public-1b"
+  }
+}
+
+resource "aws_subnet" "private-1b" {
+  vpc_id            = aws_vpc.vpc1.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "ap-southeast-1b"
+  tags = {
+    Name = "private-1b"
+  }
+}
