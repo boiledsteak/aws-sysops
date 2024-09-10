9f1598f59d87bcc80ef2f549285c6537916fbabe
boiledsteak <timothymxa@gmail.com>
2024-09-10 16:53:31 +0800

    create secgrp

---
+++ make-secgrp.tf
@@ -0,0 +1,36 @@
+resource "aws_security_group" "webaccess" {
+  name        = "webaccess"
+  description = "Security group to allow SSH access and all outbound traffic"
+  vpc_id      = aws_vpc.vpc1.id  # Reference your VPC
+
+  # Inbound Rule: Allow SSH access (port 22)
+  ingress {
+	description = "Allow SSH access"
+	from_port   = 22
+	to_port     = 22
+	protocol    = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]  # Allows SSH from anywhere; restrict this based on your requirements
+  }
+
+  # Inbound Rule: Allow HTTP access (port 80)
+  ingress {
+	description = "Allow HTTP access"
+	from_port   = 80
+	to_port     = 80
+	protocol    = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]  # Allows HTTP from anywhere; restrict if needed
+  }
+
+  # Outbound Rule: Allow all outbound traffic
+  egress {
+	description = "Allow all outbound traffic"
+	from_port   = 0
+	to_port     = 0
+	protocol    = "-1"  # "-1" means all protocols
+	cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+	Name = "webaccess"
+  }
+}
