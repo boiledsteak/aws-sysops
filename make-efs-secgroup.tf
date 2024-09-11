eb6d7f143fb00610d1ce47aa687560d3687520f4
boiledsteak <timothymxa@gmail.com>
2024-09-11 13:18:04 +0800

    try this strat

--- make-efs-secgroup.tf
+++ make-efs-secgroup.tf
@@ -1,28 +1,10 @@
-# Create a security group for EFS
+# Create the security group
 resource "aws_security_group" "efs_sg" {
   name        = "efs-security-group"
-  description = "Allow NFS traffic for EFS within the same security group and SSH access"
-  vpc_id      = "vpc-0bb5af86ff040957f"  # Replace with your VPC ID reference if needed
+  description = "Security group for EFS and SSH"
+  vpc_id      = aws_vpc.vpc1.id
 
-  # Inbound rule: Allow NFS traffic (port 2049) for members of the same security group
-  ingress {
-    description = "Allow NFS traffic within the same security group"
-    from_port   = 2049
-    to_port     = 2049
-    protocol    = "tcp"
-    security_groups = [aws_security_group.efs_sg.id]  # Reference to the same security group
-  }
-
-  # Inbound rule: Allow SSH traffic (port 22) from anywhere
-  ingress {
-    description = "Allow SSH access from anywhere"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # This allows access from any IP
-  }
-
-  # Outbound rule: Allow all outbound traffic
+  # Allow all outbound traffic
   egress {
     from_port   = 0
     to_port     = 0
@@ -36,8 +18,22 @@ resource "aws_security_group" "efs_sg" {
 }
 
 
-# attach ENI of myinstance to the security group for EFS  
-resource "aws_network_interface_sg_attachment" "sg_attachment" {
-  security_group_id    = aws_security_group.efs_sg.id
-  network_interface_id = "eni-06c4e0f34715cad95"
+# Allow NFS traffic within the same security group
+resource "aws_security_group_rule" "efs_nfs_ingress" {
+  type                     = "ingress"
+  from_port                = 2049
+  to_port                  = 2049
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.efs_sg.id
+  source_security_group_id = aws_security_group.efs_sg.id
+}
+
+# Allow SSH access from anywhere
+resource "aws_security_group_rule" "ssh_ingress" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  security_group_id = aws_security_group.efs_sg.id
+  cidr_blocks       = ["0.0.0.0/0"]
 }
